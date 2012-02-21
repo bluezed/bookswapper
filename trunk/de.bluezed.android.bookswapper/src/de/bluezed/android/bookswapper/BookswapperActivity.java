5ef1d4e569da0e97fecd72b42cd33e5202d11ff1
bluezed <bluezed@596e6c2e-6a12-48fc-8418-9ee6bcf6ab3e>
2012-02-21 14:35:15 +0000

    verion 1.0

--- trunk/de.bluezed.android.bookswapper/src/de/bluezed/android/bookswapper/BookswapperActivity.java
+++ trunk/de.bluezed.android.bookswapper/src/de/bluezed/android/bookswapper/BookswapperActivity.java
@@ -1,57 +1,129 @@
 package de.bluezed.android.bookswapper;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.StringTokenizer;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.json.JsonHttpRequest;
+import com.google.api.client.http.json.JsonHttpRequestInitializer;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.books.Books;
+import com.google.api.services.books.BooksRequest;
+import com.google.api.services.books.Books.Volumes.List;
+import com.google.api.services.books.model.Volume;
+import com.google.api.services.books.model.VolumeVolumeInfo;
+import com.google.api.services.books.model.Volumes;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BookswapperActivity extends Activity {
     
+	private String KEY = null;
 	private EditText textISBN;
 	private EditText textTitle;
 	private EditText textAuthor;
 	private EditText textPublisher;
 	private EditText textSummary;
+	private EditText textPublished;
+	private EditText textPages;
+	private ImageView imageCover;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        KEY				= this.getString(R.string.google_api_key);
         textISBN  		= (EditText) findViewById(R.id.editText1);
         textTitle 		= (EditText) findViewById(R.id.editText2);
         textAuthor 		= (EditText) findViewById(R.id.editText3);
         textPublisher 	= (EditText) findViewById(R.id.editText4);
         textSummary 	= (EditText) findViewById(R.id.editText5);
+        textPublished 	= (EditText) findViewById(R.id.editText8);
+        textPages 	= (EditText) findViewById(R.id.editText7);
+        imageCover		= (ImageView) findViewById(R.id.imageView1);
+        
+        TextView feedback = (TextView) findViewById(R.id.textView1);
+        feedback.setText(Html.fromHtml("<a href=\"" + this.getString(R.string.feedback_link) + "\">" + this.getString(R.string.copyright) + "</a>"));
+        feedback.setMovementMethod(LinkMovementMethod.getInstance());
+        
+        checkNetworkStatus();
+    }
+    
+    private boolean checkNetworkStatus() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        
+        Boolean isOnline = false;
+        
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            isOnline = true;
+        }
+        
+        if (!isOnline) {
+        	showAlert(this.getString(R.string.warning), this.getString(R.string.not_connected), this.getString(R.string.ok));
+        }
+        
+        return isOnline;
+    }
+
+    private void showAlert(String title, String message, String buttonText) {
+    	final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+    	alertDialog.setTitle(title);
+    	alertDialog.setMessage(message);
+    	alertDialog.setButton(buttonText, new DialogInterface.OnClickListener() {
+    	   public void onClick(DialogInterface dialog, int which) {
+    	      alertDialog.cancel();
+    	   }
+    	});
+    	alertDialog.setIcon(R.drawable.book_icon);
+    	alertDialog.show();
+    }
+    
+    private void clearFields() {
+    	textTitle.setText("");
+    	textAuthor.setText("");
+    	textPublisher.setText("");
+    	textSummary.setText("");
+    	textPublished.setText("");
+    	textPages.setText("");
+        imageCover.setImageResource(R.drawable.empty);
     }
     
     public void onBarcodeScanClick (View view) {    	
-    	IntentIntegrator integrator = new IntentIntegrator(this);
-    	integrator.initiateScan();
+    	clearFields();
+    	if (checkNetworkStatus()) {
+	    	IntentIntegrator integrator = new IntentIntegrator(this);
+	    	integrator.initiateScan();
+    	}
     };
+    
+    public void onManualSearchClick (View view) {
+    	clearFields();
+    	if (checkNetworkStatus() && textISBN != null && textISBN.length() > 0) {
+    		doSearch(textISBN.getText().toString());
+    	}
+    }
     
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
     	IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
@@ -59,250 +131,133 @@ public class BookswapperActivity extends Activity {
             String contents = scanResult.getContents();
             // Handle successful scan
             textISBN.setText(contents);
-            
-            // Get details
-            URL url = null;
-			try {
-				url = new URL(R.string.isbndb_url + "access_key=" + R.string.isbndb_license + "&index1=isbn&value1=" + contents + "&results=texts");
-			} catch (MalformedURLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-            HttpURLConnection urlConnection = null;
-            String response = null;
-            
-			try {
-				urlConnection = (HttpURLConnection) url.openConnection();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-            try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                response = readStream(in);
-            }
-            catch (Exception e) {
-				textTitle.setText(e.getMessage());
-            }
-             finally {
-            	urlConnection.disconnect();
-
-                /* Get a SAXParser from the SAXPArserFactory. */
-                SAXParserFactory spf = SAXParserFactory.newInstance();
-                SAXParser sp = null;
-				try {
-					sp = spf.newSAXParser();
-				} catch (ParserConfigurationException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (SAXException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-
-                /* Get the XMLReader of the SAXParser we created. */
-                XMLReader xr = null;
-				try {
-					xr = sp.getXMLReader();
-				} catch (SAXException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-                /* Create a new ContentHandler and apply it to the XML-Reader*/ 
-                ExampleHandler myExampleHandler = new ExampleHandler();
-                xr.setContentHandler(myExampleHandler);
-
-                /* Parse the xml-data from our URL. */
-                InputSource inputSource = new InputSource();
-                inputSource.setEncoding("UTF-8");
-				inputSource.setCharacterStream(new StringReader(response));
-
-                /* Parse the xml-data from our URL. */
-                try {
-					xr.parse(inputSource);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SAXException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-                /* Parsing has finished. */
-
-                /* Our ExampleHandler now provides the parsed data to us. */
-                ParsedExampleDataSet parsedExampleDataSet = myExampleHandler.getParsedData();
-
-                /* Set the result to be displayed in our GUI. */
-                String title = parsedExampleDataSet.toString("title");
-                
-                if (title == null || title == "") {
-                	title = ">> No book data found! <<";                	
-                }
-                	
-                textTitle.setText(title);
-                textAuthor.setText(parsedExampleDataSet.toString("author"));
-                textPublisher.setText(parsedExampleDataSet.toString("publisher"));
-                textSummary.setText(parsedExampleDataSet.toString("summary"));
-
-        	}       
-        } 
+            doSearch(contents);
+ 		} 
  		else {
             // Handle cancel
-        	textISBN.setText("Error scanning!");
+ 			showAlert(this.getString(R.string.info), this.getString(R.string.error_scan), this.getString(R.string.ok));
         }
     }
     
-    public static String readStream(InputStream in) throws IOException {
-          StringBuilder sb = new StringBuilder();
-          BufferedReader r = new BufferedReader(new InputStreamReader(in), 1000);
+    private void doSearch (String contents) {            
+            // Get details
+            JsonFactory jsonFactory = new JacksonFactory();
+            try {
+	              String query = "isbn:" + contents;
 
-          for (String line = r.readLine(); line != null; line = r.readLine()) {
-        	  sb.append(line).append("\n");
+	              try {
+	            	  queryGoogleBooks(jsonFactory, query);
+	            	  // Success!
+	              } catch (GoogleJsonResponseException e) {
+	            	  // message already includes parsed response
+	            	  System.err.println(e.getMessage());
+	              } catch (HttpResponseException e) {
+	            	  // message doesn't include parsed response
+	            	  System.err.println(e.getMessage());
+	            	  System.err.println(e.getResponse().parseAsString());
+	              }
+            } catch (Throwable t) {
+            	t.printStackTrace();
+            }
+    }
+    
+    private void queryGoogleBooks(JsonFactory jsonFactory, String query) throws Exception {
+    	// Set up Books client.
+    	final Books books = Books.builder(new NetHttpTransport(), jsonFactory)
+		    .setApplicationName(this.getString(R.string.app_name_internal))
+		    .setJsonHttpRequestInitializer(new JsonHttpRequestInitializer() {
+	          public void initialize(JsonHttpRequest request) {
+                BooksRequest booksRequest = (BooksRequest) request;
+                booksRequest.setKey(KEY);
+              }
+            })
+            .build();
+
+        List volumesList = books.volumes().list(query);
+
+        String title 		= "";
+        String author 		= "";
+        String publisher 	= "";
+        String summary 		= "";
+        String published	= "";
+        String pages 		= "";
+        String imageLink 	= "";
+                
+        // Execute the query.
+        Volumes volumes = volumesList.execute();
+        if (volumes.getTotalItems() == 0 || volumes.getItems() == null) {
+        	showAlert(this.getString(R.string.info), this.getString(R.string.not_found), this.getString(R.string.ok));
+        	return;
+        }
+
+        // Output results
+        for (Volume volume : volumes.getItems()) {
+          VolumeVolumeInfo volumeInfo = volume.getVolumeInfo();
+
+          // Title
+          if (volumeInfo.getTitle() != null && volumeInfo.getTitle().length() > 0) {
+        	  title = volumeInfo.getTitle();
           }
-
-          in.close();
-
-          return sb.toString();
-    }
-    
-    public class ExampleHandler extends DefaultHandler {
-
-        // ===========================================================
-        // Fields
-        // ===========================================================
-
-        private boolean in_title = false;
-        private boolean in_author = false;
-        private boolean in_publisher = false;
-        private boolean in_summary = false;
-
-        private ParsedExampleDataSet myParsedExampleDataSet = new ParsedExampleDataSet();
-
-        // ===========================================================
-        // Getter & Setter
-        // ===========================================================
-
-        public ParsedExampleDataSet getParsedData() {
-            return this.myParsedExampleDataSet;
+                    
+          // Author(s).
+          java.util.List<String> authors = volumeInfo.getAuthors();
+          if (authors != null && !authors.isEmpty()) {
+            for (int i = 0; i < authors.size(); ++i) {
+              author = author + authors.get(i);
+              if (i < authors.size() - 1) {
+                author = author + ", ";
+              }
+            }
+          }
+          
+          // Publisher
+          if (volumeInfo.getPublisher() != null && volumeInfo.getPublisher().length() > 0) {
+              publisher = volumeInfo.getPublisher();
+          }
+          
+          // Published
+          if (volumeInfo.getPublishedDate() != null && volumeInfo.getPublishedDate().length() > 0) {                
+        	  published = volumeInfo.getPublishedDate();
+        	  StringTokenizer st = new StringTokenizer(published, "-");
+        	  if (st.countTokens() > 1) {
+        		  published = st.nextToken();
+        	  }
+          }
+          
+          // Pages
+          if (volumeInfo.getPageCount() != null) {
+              pages = volumeInfo.getPageCount().toString();
+          }
+          
+          // Description (if any)
+          if (volumeInfo.getDescription() != null && volumeInfo.getDescription().length() > 0) {
+            summary = volumeInfo.getDescription();
+          }          
+          
+          // Image link
+          if (volumeInfo.getImageLinks() != null) {
+        	  if (volumeInfo.getImageLinks().getThumbnail() != null && volumeInfo.getImageLinks().getThumbnail().length() > 0) {
+        		  imageLink = volumeInfo.getImageLinks().getThumbnail();  
+        	  } 
+        	  else if (volumeInfo.getImageLinks().getSmall() != null && volumeInfo.getImageLinks().getSmall().length() > 0) {
+        		  imageLink = volumeInfo.getImageLinks().getSmall();  
+        	  }
+        	  else if (volumeInfo.getImageLinks().getMedium() != null && volumeInfo.getImageLinks().getMedium().length() > 0) {
+        		  imageLink = volumeInfo.getImageLinks().getMedium();  
+        	  }        	  
+          }
         }
-
-        // ===========================================================
-        // Methods
-        // ===========================================================
-        @Override
-        public void startDocument() throws SAXException {
-            this.myParsedExampleDataSet = new ParsedExampleDataSet();
-        }
-
-        @Override
-        public void endDocument() throws SAXException {
-            // Nothing to do
-        }
-
-        /** Gets be called on opening tags like: 
-         * <tag> 
-         * Can provide attribute(s), when xml was like:
-         * <tag attribute="attributeValue">*/
-        @Override
-        public void startElement(String uri, String localName, String qName, org.xml.sax.Attributes atts) throws SAXException {
-            super.startElement(uri, localName, qName, atts);
-            if (localName.equals("Title")) {
-                this.in_title = true;
-            }
-            else if (localName.equals("AuthorsText")) {
-                this.in_author = true;
-            }
-            else if (localName.equals("PublisherText")) {
-                this.in_publisher = true;
-            }
-            else if (localName.equals("Summary")) {
-                this.in_summary = true;
-            }
-
-        } 
-
-
-        /** Gets be called on closing tags like: 
-         * </tag> */
-        @Override
-        public void endElement(String namespaceURI, String localName, String qName)
-                throws SAXException {
-        	if (localName.equals("Title")) {
-                this.in_title = false;
-            }
-            else if (localName.equals("AuthorsText")) {
-                this.in_author = false;
-            }
-            else if (localName.equals("PublisherText")) {
-                this.in_publisher = false;
-            }
-            else if (localName.equals("Summary")) {
-                this.in_summary = false;
-            }
-        }       
-
-
-        /** Gets be called on the following structure: 
-         * <tag>characters</tag> */
-        @Override
-        public void characters(char ch[], int start, int length) {
-            if(this.in_title) {
-                myParsedExampleDataSet.setExtractedTitle(new String(ch));
-            }
-            else if (this.in_author) {
-            	 myParsedExampleDataSet.setExtractedAuthor(new String(ch));
-            }
-            else if (this.in_publisher) {
-            	 myParsedExampleDataSet.setExtractedPublisher(new String(ch));
-            }
-            else if (this.in_summary) {
-            	 myParsedExampleDataSet.setExtractedSummary(new String(ch));
-            }
-        }
-    }
-
-    public class ParsedExampleDataSet {
-    	private String extractedString	 	= null;
-        private String extractedTitle 		= null;
-        private String extractedAuthor 		= null;
-        private String extractedPublisher 	= null;
-        private String extractedSummary 	= null;
         
-        public String getExtractedString() {
-            return extractedString;
-        }
-        public void setExtractedTitle(String extractedString) {
-            this.extractedTitle = extractedString;
-        }
-        public void setExtractedAuthor(String extractedString) {
-            this.extractedAuthor = extractedString;
-        }
-        public void setExtractedPublisher(String extractedString) {
-            this.extractedPublisher = extractedString;
-        }
-        public void setExtractedSummary(String extractedString) {
-            this.extractedSummary = extractedString;
-        }
-
-        public String toString(String tag){
-            if (tag == "title") {
-            	return this.extractedTitle;
-            }
-            else if (tag == "author") {
-            	return this.extractedAuthor;
-            }
-            else if (tag == "publisher") {
-            	return this.extractedPublisher;
-            }
-            else if (tag == "summary") {
-            	return this.extractedSummary;
-            }
-            else {
-            	return null;
-            }
-        }
-
+        /* Set the result to be displayed in our GUI. */
+        textTitle.setText(title);
+        textAuthor.setText(author);
+        textPublisher.setText(publisher);
+        textSummary.setText(summary);
+        textPublished.setText(published);
+        textPages.setText(pages);        
+        
+        URL newurl = new URL(imageLink); 
+        Bitmap mIcon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream()); 
+        imageCover.setImageBitmap(mIcon_val);   
     }
-
 }
